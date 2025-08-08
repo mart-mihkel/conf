316cae5031995c18748a47fe18d06d9db3ca107b
mart-mihkel <mart.mihkel.aun@gmail.com>
2025-08-08 09:04:58 +0300

    format

--- config/.zshrc
+++ config/.zshrc
@@ -17,13 +17,12 @@ function pprecmd() {
 }
 
 function tm() {
-    dir=$(fdfind -H -t=d -d=2 . ~ | fzf)
-    [[ -z $dir ]] && return
-    name=$(basename $dir | tr . _)
-    tmux new-session -A -D -c $dir -s $name
+    dir=$(fdfind -t=d -d=2 . ~ | fzf --preview 'tmux ls' --preview-window=down,25%)
+    [[ -n $dir ]] && tmux new-session -A -D -c $dir -s $(basename $dir | tr . _)
 }
 
 autoload -Uz compinit && compinit
+
 zstyle ":completion:*" menu yes select
 zstyle ":completion:*" special-dirs true
 zstyle ":completion::complete:*" gain-privileges 1
@@ -32,28 +31,19 @@ setopt list_packed
 setopt no_case_glob no_case_match
 setopt inc_append_history share_history hist_ignore_dups
 
-alias bat="batcat"
-alias fd="fdfind"
+source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 
 alias rm="rm -v"
 alias cp="cp -v"
 alias mv="mv -v"
-
-alias ls="ls --color"
-alias la="ls -A --color"
-alias ll="ls -lAh --color"
-
-alias at="source .venv/bin/activate"
-alias jn=".venv/bin/jupyter-notebook"
-alias jl=".venv/bin/jupyter-lab"
-
 alias vim="nvim"
+alias fd="fdfind"
+alias bat="batcat"
+alias ls="ls --color"
+alias grep="grep --color"
+alias ll="ls -lAh --color"
+alias jn=".venv/bin/jupyter-notebook"
+alias venv="source .venv/bin/activate"
 alias neofetch="fastfetch --config neofetch"
 alias cssh='ssh -o ProxyCommand="cloudflared access ssh --hostname %h"'
-
-export EDITOR="vim"
-export TERMINAL="foot"
-export PATH="$HOME/.local/bin:/usr/sbin:$PATH"
-
-source $HOME/.cargo/env
-source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+alias sway="XDG_CURRENT_DESKTOP=sway XDG_SCREENSHOTS_DIR=$HOME/Pictures/screenshots sway"
--- install-debian.sh
+++ install-debian.sh
@@ -1,5 +1,7 @@
 #/usr/bin/env bash
 
+# TODO: optional (prompted?) overrides to reinstall/update
+
 ESC="\033"
 FG1="${ESC}[38;5;1m"
 FG2="${ESC}[38;5;2m"
@@ -8,15 +10,15 @@ RES="${ESC}[0m"
 
 set -e
 
-
-printf "${FG5}installation starting!${RES}\n"
-
-if ! grep -qi debian /etc/os-release >/dev/null 2>&1; then
+if ! grep -qi debian /etc/os-release > /dev/null 2>&1; then
     printf "${FG1}install cancelled: only debian supported${RES}\n"
     exit 1
 fi
 
-printf "${FG5}git${RES} ${FG2}[1/9]${RES}\n"
+printf "\n${FG2}install starting!${RES}\n"
+
+printf "\n${FG5}git${RES} ${FG2}[1/9]${RES}\n"
+
 sudo apt-get -y install git
 
 if [ ! -e $HOME/.gitconfig ]; then
@@ -25,41 +27,51 @@ if [ ! -e $HOME/.gitconfig ]; then
 
     git config --global user.name $GIT_NAME
     git config --global user.email $GIT_EMAIL
-    git config --global core.editor vim
+    git config --global core.editor nvim
     git config --global pull.rebase true
     git config --global init.defaultBranch main
     git config --global url."git@github.com:".insteadOf gh:
+else
+    printf "git already configured\n"
 fi
 
 
 
-printf "${FG5}terminal${RES} ${FG2}[2/9]${RES}\n"
-sudo apt-get -y install zsh zsh-autosuggestions wget curl tmux vim man-db \
-    less foot zip unzip fzf fastfetch btop jq ripgrep fd-find bat \
-    ca-certificates
+printf "\n${FG5}terminal${RES} ${FG2}[2/9]${RES}\n"
 
-cp -r config/foot $HOME/.config
-cp config/.tmux.conf $HOME
-cp config/.vimrc $HOME
-cp config/.zshrc $HOME
+sudo apt-get -y install zsh zsh-autosuggestions wget curl tmux vim man-db \
+    less zip unzip fzf fastfetch btop jq ripgrep fd-find bat ca-certificates
+
 sudo chsh -s $(which zsh) $USER
 
+mkdir -p $HOME/.config
+
+cp -v config/.tmux.conf $HOME
+cp -v config/.profile $HOME
+cp -v config/.vimrc $HOME
+cp -v config/.zshrc $HOME
 
 
-printf "${FG5}devel${RES} ${FG2}[3/9]${RES}\n"
+
+printf "\n${FG5}devel${RES} ${FG2}[3/9]${RES}\n"
 sudo apt-get -y install gcc make cmake golang luajit nodejs npm
 
-if ! command -v uv >/dev/null 2>&1; then
+if ! command -v uv > /dev/null 2>&1; then
     curl -LsSf https://astral.sh/uv/install.sh | sh
+else
+    printf "uv already installed\n"
 fi
 
-if ! command -v rustup >/dev/null 2>&1; then
+if ! command -v rustup > /dev/null 2>&1; then
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+else
+    printf "rust already installed\n"
 fi
 
 
 
-printf "${FG5}neovim${RES} ${FG2}[4/9]${RES}\n"
+printf "\n${FG5}neovim${RES} ${FG2}[4/9]${RES}\n"
+
 if ! command -v nvim >/dev/null 2>&1; then
     NVIM_VERSION="v0.11.3"
 
@@ -73,12 +85,15 @@ if ! command -v nvim >/dev/null 2>&1; then
     cp -r config/nvim $HOME/.config
 
     $HOME/.cargo/bin/cargo install tree-sitter-cli
+else
+    printf "neovim already installed\n"
 fi
 
 
 
-printf "${FG5}docker${RES} ${FG2}[5/9]${RES}\n"
-if ! command -v docker >/dev/null 2>&1; then
+printf "\n${FG5}docker${RES} ${FG2}[5/9]${RES}\n"
+
+if ! command -v docker > /dev/null 2>&1; then
     sudo install -m 0755 -d /etc/apt/keyrings
     sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
     sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -89,36 +104,39 @@ if ! command -v docker >/dev/null 2>&1; then
       sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
     sudo apt-get update
-    sudo apt-get install -y docker-ce docker-ce-cli \
-        containerd.io docker-buildx-plugin \
-        docker-compose-plugin
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io \
+        docker-buildx-plugin docker-compose-plugin
 
     sudo usermod -aG docker $USER
+else
+    printf "docker already installed\n"
 fi
 
 
-printf "${FG5}sway${RES} ${FG2}[6/9]${RES}\n"
+printf "\n${FG5}sway${RES} ${FG2}[6/9]${RES}\n"
+
 sudo apt-get -y install sway swaybg swaylock autotiling gammastep waybar \
-    dunst tofi vlc thunar grimshot wl-clipboard brightnessctl \
-    xdg-desktop-portal xdg-desktop-portal-wlr xwayland xwaylandvideobridge \
-    dbus
+    foot dunst tofi vlc thunar grimshot wl-clipboard brightnessctl dbus \
+    xdg-desktop-portal xdg-desktop-portal-wlr xwayland xwaylandvideobridge
 
 cp -rv config/gammastep $HOME/.config
 cp -rv config/waybar $HOME/.config
 cp -rv config/dunst $HOME/.config
 cp -rv config/sway $HOME/.config
 cp -rv config/tofi $HOME/.config
+cp -rv config/foot $HOME/.config
 
 mkdir -p $HOME/Pictures/walls
-cp -v walls/* $HOME/Pictures/walls
+cp -rv walls $HOME/Pictures
 
 
 
-printf "${FG5}hardware${RES} ${FG2}[7/9]${RES}\n"
+printf "\n${FG5}hardware${RES} ${FG2}[7/9]${RES}\n"
+
 sudo apt-get -y install playerctl pipewire pipewire-pulse wireplumber \
     bluetooth bluez thermald
 
-if ! command -v intel-undervolt >/dev/null 2>&1; then
+if ! command -v intel-undervolt > /dev/null 2>&1; then
     mkdir -p $HOME/git
     git clone https://github.com/kitsunyan/intel-undervolt.git $HOME/git/intel-undervolt
 
@@ -131,9 +149,11 @@ if ! command -v intel-undervolt >/dev/null 2>&1; then
     sudo sed -i "s/^undervolt 0.*/undervolt 0 'CPU' -100/" /etc/intel-undervolt.conf
     sudo sed -i "s/^undervolt 1.*/undervolt 1 'GPU' -100/" /etc/intel-undervolt.conf
     sudo sed -i "s/^undervolt 2.*/undervolt 2 'GPU Cache' -100/" /etc/intel-undervolt.conf
+else
+    printf "intel-undervolt already installed\n"
 fi
 
-if ! command -v auto-cpufreq >/dev/null 2>&1; then
+if ! command -v auto-cpufreq > /dev/null 2>&1; then
     mkdir -p $HOME/git
     git clone https://github.com/AdnanHodzic/auto-cpufreq.git $HOME/git/auto-cpufreq
 
@@ -141,6 +161,8 @@ if ! command -v auto-cpufreq >/dev/null 2>&1; then
     sudo ./auto-cpufreq-installer
     suod auto-cpufreq --install
     cd -
+else
+    printf "auto-cpufreq already installed\n"
 fi
 
 systemctl --user enable --now pipewire pipewire-pulse wireplumber
@@ -148,7 +170,8 @@ sudo systemctl enable --now bluetooth thermald intel-undervolt
 
 
 
-printf "${FG5}fonts${RES} ${FG2}[8/9]${RES}\n"
+printf "\n${FG5}fonts${RES} ${FG2}[8/9]${RES}\n"
+
 sudo apt-get -y install fontconfig fonts-noto
 
 mkdir -p $HOME/.local/share/fonts
@@ -157,22 +180,30 @@ if ! fc-list | grep -qi "JetBrainsMono Nerd Font"; then
     cd /tmp
     wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/JetBrainsMono.zip
     unzip JetBrainsMono.zip
+    mkdir -p $HOME/.local/share/fonts
     cp JetBrainsMonoNerdFont-Regular.ttf $HOME/.local/share/fonts
     fc-cache
     cd -
+else
+    printf "jetbrains nerd font already installed\n"
 fi
 
 
 
-printf "${FG5}apps${RES} ${FG2}[9/9]${RES}\n"
-if ! command -v brave-browser >/dev/null 2>&1; then
+printf "\n${FG5}apps${RES} ${FG2}[9/9]${RES}\n"
+
+if ! command -v brave-browser > /dev/null 2>&1; then
     curl -fsS https://dl.brave.com/install.sh | sh
+else
+    printf "brave-browser already installed\n"
 fi
 
-if ! command -v discord >/dev/null 2>&1; then
+if ! command -v discord > /dev/null 2>&1; then
     wget -O discord.deb 'https://discord.com/api/download?platform=linux&format=deb'
     sudo apt-get install -y ./discord.deb
     rm discord.deb
+else
+    printf "discord already installed\n"
 fi
 
-printf "${FG2}installation complete!${RES}\n"
+printf "\n${FG2}install finished!${RES}\n"
