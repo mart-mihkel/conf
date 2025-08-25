3713e125a9bf32a8e3ede1ac95d571d9c9974c00
mart-mihkel <mart.mihkel.aun@gmail.com>
2025-08-25 10:39:30 +0300

    dumb down

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -4,8 +4,8 @@ vim.o.clipboard = "unnamedplus"
 vim.o.relativenumber = true
 vim.o.termguicolors = true
 vim.o.winborder = "single"
-vim.o.signcolumn = "yes"
 vim.o.colorcolumn = "80"
+vim.o.signcolumn = "yes"
 vim.o.ignorecase = true
 vim.o.cursorline = true
 vim.o.expandtab = true
@@ -21,21 +21,19 @@ vim.o.list = true
 vim.o.tabstop = 4
 
 vim.pack.add({
-    "https://github.com/vague2k/vague.nvim",
-    "https://github.com/echasnovski/mini.pick",
-    "https://github.com/stevearc/conform.nvim",
-    "https://github.com/neovim/nvim-lspconfig",
-    "https://github.com/lewis6991/gitsigns.nvim",
-    "https://github.com/williamboman/mason.nvim",
-    "https://github.com/NMAC427/guess-indent.nvim",
-    "https://github.com/nvim-treesitter/nvim-treesitter",
-    "https://github.com/williamboman/mason-lspconfig.nvim",
-    { src = "https://github.com/saghen/blink.cmp", version = "v1.6.0" },
+	"https://github.com/vague2k/vague.nvim",
+	"https://github.com/echasnovski/mini.pick",
+	"https://github.com/stevearc/conform.nvim",
+	"https://github.com/neovim/nvim-lspconfig",
+	"https://github.com/lewis6991/gitsigns.nvim",
+	"https://github.com/williamboman/mason.nvim",
+	"https://github.com/NMAC427/guess-indent.nvim",
+	"https://github.com/nvim-treesitter/nvim-treesitter",
+	"https://github.com/williamboman/mason-lspconfig.nvim",
+	{ src = "https://github.com/saghen/blink.cmp", version = "v1.6.0" },
 })
 
 vim.cmd.colorscheme("vague")
-vim.lsp.config("*", { root_markers = { ".git" } })
-vim.diagnostic.config({ virtual_text = { current_line = true } })
 
 require("mason").setup()
 require("gitsigns").setup()
@@ -44,44 +42,43 @@ require("blink.cmp").setup()
 require("guess-indent").setup()
 require("mason-lspconfig").setup()
 require("nvim-treesitter.configs").setup({
-    highlight = { enable = true },
-    auto_install = true,
+	highlight = { enable = true },
+	auto_install = true,
 })
 
 require("conform").setup({
-    default_format_opts = { lsp_format = "fallback" },
-    formatters_by_ft = {
-        json = { "jq" },
-        lua = { "stylua" },
-        nix = { "alejandra" },
-        css = { "prettierd" },
-        html = { "prettierd" },
-        python = { "ruff_format" },
-        typescript = { "prettierd" },
-        typescriptreact = { "prettierd" },
-    },
+	default_format_opts = { lsp_format = "fallback" },
+	formatters_by_ft = {
+		json = { "jq" },
+		lua = { "stylua" },
+		nix = { "alejandra" },
+		css = { "prettierd" },
+		html = { "prettierd" },
+		python = { "ruff_format" },
+		typescript = { "prettierd" },
+		typescriptreact = { "prettierd" },
+	},
 })
 
-vim.keymap.set({ "n", "v" }, "<C-d>", "<C-d>zz")
-vim.keymap.set({ "n", "v" }, "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "<C-k>", ":cprevious<CR>")
-vim.keymap.set("n", "<C-j>", ":cnext<CR>")
-vim.keymap.set("n", "<M-t>", ":tabnew %<CR>")
 vim.keymap.set("n", "<M-1>", "1gt")
 vim.keymap.set("n", "<M-2>", "2gt")
 vim.keymap.set("n", "<M-3>", "3gt")
 vim.keymap.set("n", "<M-4>", "4gt")
+vim.keymap.set("n", "<M-t>", ":tabnew %<CR>")
+vim.keymap.set("n", "<C-j>", ":cnext<CR>")
+vim.keymap.set("n", "<C-k>", ":cprevious<CR>")
+vim.keymap.set({ "n", "v" }, "<C-d>", "<C-d>zz")
+vim.keymap.set({ "n", "v" }, "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "gd", vim.lsp.buf.definition)
-vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
 vim.keymap.set("n", "<leader>gf", require("conform").format)
 vim.keymap.set("n", "<leader>gr", require("gitsigns").reset_hunk)
 vim.keymap.set("n", "<leader>gp", require("gitsigns").preview_hunk)
 vim.keymap.set("n", "<leader>sr", require("mini.pick").builtin.resume)
 vim.keymap.set("n", "<leader>sg", require("mini.pick").builtin.grep_live)
 vim.keymap.set("n", "<leader>sf", function()
-    if vim.uv.fs_stat(".git") then
-        require("mini.pick").builtin.files({ tool = "git" })
-    else
-        require("mini.pick").builtin.files()
-    end
+	if vim.uv.fs_stat(".git") then
+		require("mini.pick").builtin.files({ tool = "git" })
+	else
+		require("mini.pick").builtin.files()
+	end
 end)
