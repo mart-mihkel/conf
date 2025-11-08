b519b8e7181a4ee76c48b62183748bda65470cc8
mart-mihkel <mart.mihkel.aun@gmail.com>
2025-11-08 13:38:30 +0200

    align vim and neovim configs

--- .vimrc
+++ .vimrc
@@ -1,21 +1,26 @@
 let mapleader=' '
 let g:netrw_banner=0
+let g:netrw_preview=1
+
 set viminfofile=~/.vim/info
 set backupdir=~/.vim
 set directory=~/.vim
 set undodir=~/.vim
 set encoding=utf-8
 set relativenumber
+set termguicolors
 set nocompatible
 set shiftwidth=4
 set scrolloff=4
 set autoindent
-set copyindent
 set ignorecase
 set noswapfile
+set splitright
+set splitbelow
 set expandtab
 set smartcase
 set incsearch
+set tabstop=4
 set smarttab
 set undofile
 set path+=**
@@ -25,8 +30,28 @@ set hidden
 syntax on
 filetype plugin on
 
-nnoremap <space> <nop>
-nnoremap <leader>gf :%s/\s\+$//e<CR>
-nnoremap <leader>sf :find<space>
 nnoremap Y y$
 nnoremap D d$
+
+nnoremap j gj
+nnoremap k gk
+vnoremap j gj
+vnoremap k gk
+
+nnoremap <c-d> <c-d>zz
+nnoremap <c-u> <c-u>zz
+vnoremap <c-d> <c-d>zz
+vnoremap <c-u> <c-u>zz
+
+nnoremap <c-n> :cnext<cr>
+nnoremap <c-p> :cprevious<cr>
+nnoremap <c-t> :tabnew %<cr>
+nnoremap <c-h> 1gt
+nnoremap <c-j> 2gt
+nnoremap <c-k> 3gt
+nnoremap <c-l> 4gt
+
+nnoremap <space> <nop>
+nnoremap <leader>gf :%s/\s\+$//e<cr>
+nnoremap <leader>fo :browse oldfiles<cr>
+nnoremap <leader>ff :find<space>
--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,5 +1,7 @@
 vim.g.mapleader = " "
-vim.g.netrw_banner = false
+vim.g.netrw_banner = 0
+vim.g.netrw_preview = 1
+
 vim.o.clipboard = "unnamedplus"
 vim.o.relativenumber = true
 vim.o.termguicolors = true
@@ -8,6 +10,8 @@ vim.o.colorcolumn = "80"
 vim.o.signcolumn = "yes"
 vim.o.ignorecase = true
 vim.o.cursorline = true
+vim.o.splitright = true
+vim.o.splitbelow = true
 vim.o.expandtab = true
 vim.o.smartcase = true
 vim.o.swapfile = false
@@ -23,14 +27,12 @@ vim.o.tabstop = 4
 
 vim.pack.add({
 	"https://github.com/saghen/blink.cmp",
-	"https://github.com/j-hui/fidget.nvim",
 	"https://github.com/vague2k/vague.nvim",
 	"https://github.com/nvim-lua/plenary.nvim",
 	"https://github.com/stevearc/conform.nvim",
 	"https://github.com/neovim/nvim-lspconfig",
 	"https://github.com/lewis6991/gitsigns.nvim",
 	"https://github.com/williamboman/mason.nvim",
-	"https://github.com/folke/todo-comments.nvim",
 	"https://github.com/NMAC427/guess-indent.nvim",
 	"https://github.com/rafamadriz/friendly-snippets",
 	"https://github.com/nvim-telescope/telescope.nvim",
@@ -39,11 +41,9 @@ vim.pack.add({
 })
 
 require("mason").setup()
-require("fidget").setup()
 require("blink.cmp").setup()
 require("guess-indent").setup()
 require("mason-lspconfig").setup()
-require("todo-comments").setup({ signs = false })
 
 require("nvim-treesitter.configs").setup({
 	highlight = { enable = true },
@@ -62,6 +62,7 @@ require("gitsigns").setup({
 
 require("telescope").setup({
 	defaults = {
+		borderchars = { " ", " ", " ", " ", " ", " ", " ", " " },
 		layout_config = {
 			horizontal = {
 				width = { padding = 0 },
@@ -75,7 +76,6 @@ require("conform").setup({
 	default_format_opts = { lsp_format = "fallback" },
 	formatters_by_ft = {
 		json = { "jq" },
-		jsonc = { "jq" },
 		lua = { "stylua" },
 		nix = { "alejandra" },
 		css = { "prettierd" },
@@ -96,14 +96,14 @@ vim.keymap.set({ "n", "v" }, "j", "gj")
 vim.keymap.set({ "n", "v" }, "k", "gk")
 vim.keymap.set({ "n", "v" }, "<c-d>", "<c-d>zz")
 vim.keymap.set({ "n", "v" }, "<c-u>", "<c-u>zz")
+vim.keymap.set({ "n", "v" }, "<c-n>", ":cnext<cr>")
+vim.keymap.set({ "n", "v" }, "<c-p>", ":cprevious<cr>")
 
-vim.keymap.set("n", "<c-n>", ":cnext<cr>")
-vim.keymap.set("n", "<c-p>", ":cprevious<cr>")
-vim.keymap.set("n", "<c-t>", ":tabnew %<cr>")
 vim.keymap.set("n", "<c-h>", "1gt")
 vim.keymap.set("n", "<c-j>", "2gt")
 vim.keymap.set("n", "<c-k>", "3gt")
 vim.keymap.set("n", "<c-l>", "4gt")
+vim.keymap.set("n", "<c-t>", ":tabnew %<cr>")
 
 vim.keymap.set("n", "gd", vim.lsp.buf.definition)
 vim.keymap.set("n", "<leader>gf", require("conform").format)
@@ -115,6 +115,7 @@ vim.keymap.set("n", "<leader>gp", require("gitsigns").preview_hunk)
 vim.keymap.set("n", "<leader>fr", require("telescope.builtin").resume)
 vim.keymap.set("n", "<leader>fo", require("telescope.builtin").oldfiles)
 vim.keymap.set("n", "<leader>fg", require("telescope.builtin").live_grep)
+vim.keymap.set("n", "<leader>fs", require("telescope.builtin").git_status)
 vim.keymap.set("n", "<leader>ff", function()
 	if vim.uv.fs_stat(".git") then
 		require("telescope.builtin").git_files({ show_untracked = true })
