b6f6ed3f98e88f71ef08eced2fa7747464c7b0d7
mart-mihkel <mart.mihkel.aun@gmail.com>
2026-03-13 21:27:49 +0200

    keymaps

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -18,25 +18,23 @@ vim.o.swapfile = false
 vim.o.hlsearch = false
 vim.o.undofile = true
 vim.o.laststatus = 3
-vim.o.cmdheight = 0
 vim.o.scrolloff = 4
 vim.o.number = true
 vim.o.wrap = true
 vim.o.list = true
 
 vim.pack.add({
-	{ src = "https://github.com/saghen/blink.cmp" },
-	{ src = "https://github.com/nvim-lua/plenary.nvim" },
-	{ src = "https://github.com/stevearc/conform.nvim" },
-	{ src = "https://github.com/neovim/nvim-lspconfig" },
-	{ src = "https://github.com/lewis6991/gitsigns.nvim" },
-	{ src = "https://github.com/williamboman/mason.nvim" },
-	{ src = "https://github.com/NMAC427/guess-indent.nvim" },
-	{ src = "https://github.com/projekt0n/github-nvim-theme" },
-	{ src = "https://github.com/rafamadriz/friendly-snippets" },
-	{ src = "https://github.com/nvim-telescope/telescope.nvim" },
-	{ src = "https://github.com/williamboman/mason-lspconfig.nvim" },
-	{ src = "https://github.com/saghen/blink.cmp", version = "v1.8.0" },
+	"https://github.com/nvim-lua/plenary.nvim",
+	"https://github.com/stevearc/conform.nvim",
+	"https://github.com/neovim/nvim-lspconfig",
+	"https://github.com/lewis6991/gitsigns.nvim",
+	"https://github.com/williamboman/mason.nvim",
+	"https://github.com/nmac427/guess-indent.nvim",
+	"https://github.com/projekt0n/github-nvim-theme",
+	"https://github.com/rafamadriz/friendly-snippets",
+	"https://github.com/nvim-telescope/telescope.nvim",
+	"https://github.com/williamboman/mason-lspconfig.nvim",
+	{ src = "https://github.com/saghen/blink.cmp", version = "v1.9.1" },
 })
 
 require("mason").setup()
@@ -59,39 +57,33 @@ require("gitsigns").setup({
 	signs = {
 		add = { text = "+" },
 		change = { text = "~" },
-		delete = { text = "-" },
+		delete = { text = "_" },
 		topdelete = { text = "-" },
 		changedelete = { text = "~" },
 	},
 })
 
 require("conform").setup({
+	default_format_opts = { lsp_format = "fallback" },
+	formatters = { typstyle = { prepend_args = { "--wrap-text" } } },
 	formatters_by_ft = {
-		lua = { "stylua" },
-		nix = { "alejandra" },
-		typst = { "typstyle" },
-		python = { "ruff_format" },
-		markdown = { "prettierd" },
-
 		css = { "biome" },
 		html = { "biome" },
 		json = { "biome" },
-		jsonc = { "biome" },
+		lua = { "stylua" },
+		nix = { "alejandra" },
+		typst = { "typstyle" },
 		javascript = { "biome" },
 		typescript = { "biome" },
+		python = { "ruff_format" },
+		markdown = { "prettierd" },
 		javascriptreact = { "biome" },
 		typescriptreact = { "biome" },
 	},
-	formatters = {
-		typstyle = {
-			prepend_args = { "--wrap-text" },
-		},
-	},
-	default_format_opts = { lsp_format = "fallback" },
 })
 
 vim.cmd.colorscheme("github_light")
-vim.api.nvim_set_hl(0, "StatusLine", { bg = "none", fg = "none" })
+vim.api.nvim_set_hl(0, "StatusLine", {})
 
 vim.keymap.set({ "n", "v" }, "j", "gj")
 vim.keymap.set({ "n", "v" }, "k", "gk")
@@ -100,15 +92,13 @@ vim.keymap.set("n", "<c-p>", ":silent! cprevious<cr>")
 
 vim.keymap.set("n", "gd", vim.lsp.buf.definition)
 vim.keymap.set("n", "<leader>gf", require("conform").format)
-
+vim.keymap.set("n", "<leader>gb", require("gitsigns").blame_line)
+vim.keymap.set("n", "<leader>gr", require("gitsigns").reset_hunk)
+vim.keymap.set("n", "<leader>gs", require("gitsigns").stage_hunk)
+vim.keymap.set("n", "<leader>gp", require("gitsigns").preview_hunk)
 vim.keymap.set("n", "<leader>fr", require("telescope.builtin").resume)
 vim.keymap.set("n", "<leader>fo", require("telescope.builtin").oldfiles)
 vim.keymap.set("n", "<leader>fg", require("telescope.builtin").live_grep)
+vim.keymap.set("n", "<leader>fh", require("telescope.builtin").help_tags)
 vim.keymap.set("n", "<leader>fs", require("telescope.builtin").git_status)
-vim.keymap.set("n", "<leader>ff", function()
-	if vim.uv.fs_stat(".git") then
-		require("telescope.builtin").git_files({ show_untracked = true })
-	else
-		require("telescope.builtin").find_files()
-	end
-end)
+vim.keymap.set("n", "<leader>ff", require("telescope.builtin").find_files)
