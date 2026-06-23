d354d82814471eb71fec55bd32ae677ba0a56cc3
mart-mihkel <mart.mihkel.aun@gmail.com>
2026-06-23 18:22:28 +0300

    ordered wallpapers

--- scripts/install-post.sh
+++ scripts/install-post.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 log()  { printf "\033[1;34m[%s]\033[0m %s\n" "$(date '+%H:%M:%S')" "$*"; }
 warn() { printf "\033[1;33m[%s]\033[0m %s\n" "$(date '+%H:%M:%S')" "$*"; }
 
-WALLPAPER=~/.cache/rice/walls/light-moomin-fishing.png
+WALLPAPER=~/.cache/rice/walls/01-dude-light.jpg
 
 if command -v matugen &>/dev/null; then
     log "Generating colors"
